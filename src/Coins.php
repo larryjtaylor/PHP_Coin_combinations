8788f440cff96542a61bde70440070440d7e5251
Larry Taylor and Max Scher <larry@>
2017-07-05 11:22:40 -0700

    refactor Coins.php to include  variables

--- src/Coins.php
+++ src/Coins.php
@@ -5,10 +5,17 @@
         function makeCoins($input)
         {
             $coins_arr = ['quarters' => 0, 'dimes' => 0, 'nickels' => 0, 'pennies' => 0];
+
             $quarters = intval($input / 25);
-            $dimes = intval(($input - ($quarters * 25)) / 10);
-            $nickels = intval(($input - ($quarters * 25) - ($dimes * 10)) / 5);
-            $pennies = intval(($input - ($quarters * 25) - ($dimes * 10)) - ($nickels * 5));
+            $quarters_val = $quarters * 25;
+
+            $dimes = intval(($input - $quarters_val ) / 10);
+            $dimes_val = $dimes * 10;
+
+            $nickels = intval(($input - $quarters_val - $dimes_val) / 5);
+            $nickels_val = $nickels * 5;
+
+            $pennies = intval($input - $quarters_val - $dimes_val - $nickels_val);
 
             $coins_arr['quarters'] = $quarters;
             $coins_arr['dimes'] = $dimes;
