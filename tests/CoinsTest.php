d6972506a517eaec5dd3cbd01ada6adc36410afc
Larry Taylor and Max Scher <larry@>
2017-07-05 09:05:49 -0700

    add entry form for cents

---
+++ tests/CoinsTest.php
@@ -0,0 +1,16 @@
+<?php
+    require_once '/../src/Coins.php';
+
+    class CoinsTest extends PHPUnit_Framework_TestCase
+    {
+        function test_makeCoins()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            
+        }
+    }
+?>
