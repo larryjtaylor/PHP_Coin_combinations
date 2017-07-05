6f6337a648a8dcf567e3634832716989968b28b4
Larry Taylor and Max Scher <larry@>
2017-07-05 09:18:59 -0700

    pass penny test

--- src/Coins.php
+++ src/Coins.php
@@ -2,9 +2,9 @@
 
     class Coins
     {
-        function __construct($input)
+        function makeCoins($input)
         {
-            
+            return $input . ' pennies';
         }
     }
 
--- tests/CoinsTest.php
+++ tests/CoinsTest.php
@@ -1,16 +1,19 @@
 <?php
-    require_once '/../src/Coins.php';
+    require_once 'src/Coins.php';
 
     class CoinsTest extends PHPUnit_Framework_TestCase
     {
         function test_makeCoins()
         {
             //Arrange
+            $test_cents = new Coins;
+            $input = 43;
 
             //Act
+            $result = $test_cents->makeCoins($input);
 
             //Assert
-            
+            $this->assertEquals(43 . ' pennies', $result);
         }
     }
 ?>
