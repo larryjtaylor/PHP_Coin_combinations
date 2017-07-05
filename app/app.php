4b09802cbcf1d219788ecd4973934cc081d32414
Larry Taylor and Max Scher <larry@>
2017-07-05 08:54:08 -0700

    add base code for app.php

--- app/app.php
+++ app/app.php
@@ -1,5 +1,13 @@
 <?php
-    date_default_timezone_set("America/Los_Angeles");
-    require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Coins.php";
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Coins.php';
+
+    $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app->get('/', function() use ($app) {
+
+        return $app['twig']->render('coins.html.twig');
+    });
 ?>
